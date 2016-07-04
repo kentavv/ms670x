c5ed02e5e57df411f615cf6c89b636e31811120a
kentavv <kent.vandervelden@gmail.com>
2016-07-03 19:36:41 -0500

    Increased the serial timeout to overlook a slightly slow response.
    Added code to discard the intial bytes until valid start-end bytes are seen.
    
    Decode date-time string into a date-time object.
    
    Added code to handle an apparent firmware bug that inverts the meaning of
    the frequency weights when sending pre-recorded data.
    
    Added several notes regarding problems with pre-recorded data, and verified
    many of the known problems against the Mastech v2.1 software.

--- ms670x.py
+++ ms670x.py
@@ -34,7 +34,7 @@
 # The '30-130' is actually an 'auto-range' range. The manual warns this mode
 # is 'unfit for measuring a[n] instantaneous and impactive noises' (pg. 10)
 #
-# The over-underwarning is sent from the meter, but could be constructed
+# The over-under warning is sent from the meter, but could be constructed
 # from the current range sent in live and pre-recorded measurements.
 #
 # The meter does not need to already be in RS232 mode to send pre-recorded data.
@@ -43,22 +43,23 @@
 #
 
 import serial
+from datetime import datetime
 
 ser = serial.Serial(port='/dev/ttyUSB0', baudrate=2400, bytesize=serial.EIGHTBITS, 
                     parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE, 
-                    timeout=1, xonxoff=False, rtscts=False, dsrdtr=False)
+                    timeout=1.5, xonxoff=False, rtscts=False, dsrdtr=False)
 
 ser.flushInput()
 ser.flushOutput()
 
-st = 0
+st = -1 # discard bytes until reading first start-end byte
 measurement_range = 0
 rc_f = ''
 
 while True:
-  # Read a single byte at a time until syncing with a start byte, then change 
+  # Read a single byte at a time until syncing with a start-end byte, then change 
   # state to read a message, and repeat.
-  if st == 0:
+  if st == 0 or st == -1:
     c = ser.read()
     if c == '':
       print 'unable to read from device'
@@ -89,12 +90,11 @@ while True:
       st = 1
       rc_f = 'pre-recorded'
       measurement_range = c
-    elif c == 141: # only seen once... was it actually seen?
-      print 'Done reading pre-recorded measurements (response=141)'
     elif c == 144:
-      print 'Done reading pre-recorded measurements (response=144)'
+      print 'Done reading pre-recorded measurements'
     else:
-      print 'Unknown start-end flag:', c
+      if st != -1:
+        print 'Unknown start-end flag:', c
     if st == 1:
       measurement_range = {128:[30, 80],  129:[40, 90],  130:[50, 100], 
                            131:[60, 110], 132:[70, 120], 133:[80, 130], 
@@ -142,6 +142,9 @@ while True:
     full_warning = {0:'', 1:'mem-full'}[full_warning]
 
     frequency_weighting = (dat[0] >> 3) & 1
+    if rc_f == 'pre-recorded':
+      # the meaning of frequency_weight flag appears to be inverted when reading pre-recorded data
+      frequency_weighting = 1 - frequency_weighting
     frequency_weighting = {0:'C', 1:'A'}[frequency_weighting]
 
     time_weighting = (dat[0] >> 2) & 1
@@ -158,9 +161,90 @@ while True:
     dt = '20{0:d}{1:d}-{2:d}{3:d}-{4:d}{5:d}'.format(*dat[5:11])
     tm = '{0:d}{1:d}:{2:d}{3:d}:{4:d}{5:d}'.format(*dat[11:17])
 
+    dt_tm = dt + ' ' + tm
+
+    # enable if you want to do date-time validation or other calculations
+    try:
+      dt_tm = datetime.strptime(dt_tm, '%Y-%m-%d %H:%M:%S')
+    except:
+      print 'Could not convert date-time:', dt_tm
+      continue
+
     ou_warning = under_warning + over_warning
  
-    print rc_f, dt, tm, db, 'db'+frequency_weighting, time_weighting, \
+    print rc_f, dt_tm, db, 'db'+frequency_weighting, time_weighting, \
           measurement_range, ou_warning, max_enabled, ob_warning, \
           full_warning, low_battery
  
+
+# Sending pre-recorded data seems to have some problems...
+#
+# 1. The meaning of the frequency weighting flag is inverted, but that we correct
+# in software.
+#
+# 2. It is possible for time points to repeat in recorded data, seems to occur when changing
+# frequency weighting modes.
+#
+# pre-recorded 2016-07-03 19:01:12 51.7 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:13 51.8 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:14 51.8 dbA slow(1s) [40, 90]     <----
+# pre-recorded 2016-07-03 19:01:15 51.8 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:16 51.7 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:17 51.7 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:18 51.7 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:19 51.7 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:20 51.6 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:21 51.7 dbA slow(1s) [40, 90]     <----
+# pre-recorded 2016-07-03 19:01:14 55.1 dbC slow(1s) [40, 90]     <---- being repeating
+# pre-recorded 2016-07-03 19:01:15 57.5 dbC slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:16 58.3 dbC slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:17 58.8 dbC slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:18 58.5 dbC slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:19 58.3 dbC slow(1s) [40, 90] 
+# pre-recorded 2016-07-03 19:01:20 58.3 dbC slow(1s) [40, 90]
+# pre-recorded 2016-07-03 19:01:21 58.6 dbC slow(1s) [40, 90]     <---- end repeating
+# pre-recorded 2016-07-03 19:01:22 59.0 dbC slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:01:23 58.5 dbC slow(1s) [40, 90]     
+#
+# 3. Sometimes the pre-recorded data can have a future timestamp.
+#  
+# pre-recorded 2016-07-03 19:12:02 56.5 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:12:03 56.3 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:12:04 56.2 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:12:05 55.8 dbA slow(1s) [40, 90]     
+# pre-recorded 2016-07-03 19:12:06 55.4 dbA slow(1s) [40, 90]     <---- future timestamp
+# pre-recorded 2016-07-03 19:06:53 60.6 dbC slow(1s) [40, 90]     
+# ....
+# pre-recorded 2016-07-03 19:07:03 53.1 dbA slow(1s) [40, 90]     <---- timestamp from near when send was pressed
+# Done reading pre-recorded measurements
+# live 2016-07-03 19:08:02 53.2 dbA slow(1s) [40, 90]     <---- timestamp now
+#
+#
+# Some of these and others, such as the day being offset by 40, were verified
+# to be a problem with the Mastech software as well. Below is a sample report
+# from the Mastech software. Unfortunately, it does not save fully decoded
+# packets, so yet another reason to write ones own software.
+#
+#
+#
+#Sound Level Meter Data Report   (07/03/2016/19:19:11/422)               
+#                
+#Record  Date    Time    Range   Value (dB)
+#...
+#310 16-07-43    19:16:03    40-90   60
+#311 16-07-43    19:16:04    40-90   59.4
+#312 16-07-43    19:16:05    40-90   58.8        <----
+#313 16-07-43    19:16:06    40-90   60
+#314 16-07-43    19:16:07    40-90   59.3
+#315 16-07-43    19:16:08    40-90   59.4
+#316 16-07-43    19:16:09    40-90   59.9
+#317 16-07-43    19:16:10    40-90   60.1        <---- 
+#318 16-07-43    19:16:05    40-90   59.6        <---- repeats begin
+#319 16-07-43    19:16:06    40-90   57.9
+#320 16-07-43    19:16:07    40-90   56.7
+#321 16-07-43    19:16:08    40-90   56.4
+#322 16-07-43    19:16:09    40-90   56          <---- repeats end
+#323 16-07-43    19:16:10    40-90   55.5
+#324 16-07-43    19:16:11    40-90   55.5
+#325 16-07-43    19:16:12    40-90   55.5
+#
